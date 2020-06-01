c9a4212ced86f62ddfb4dddd667d32847bb560cd
bahayg <bahayg@gmail.com>
2020-05-31 23:29:32 -0700

    Update valid parentheses for blog post

--- LeetCode/validParentheses.js
+++ LeetCode/validParentheses.js
@@ -71,6 +71,7 @@ isValid = s => {
 // Runtime: 68 ms / Memory: 33.3 MB
 
 isValid = s =>  {
+  // Create a hash
     let map = {
         ")": "(",
         "}": "{",
@@ -108,3 +109,44 @@ isValid = s =>  {
 //   stck.empty? 
 // end
 
+
+
+// For my blog post:
+
+"" returns true
+"()" returns true
+"()[]{}" returns true
+"(]" returns false
+"([)]" returns false
+"{[]}" returns true
+
+
+
+
+
+"({{(([[((({ Make sure that I am balanced! ]))))}}]}}]"
+
+
+
+
+
+isValid = s => {
+    // Create a stack
+    let stck = []
+    // Loop through each element in the string
+    for (i = 0; i < s.length; i ++) {
+      let char = stck[stck.length -1]
+      // If you encounter a starting bracket, push it onto the stack
+      if (s[i] == "(" || s[i] == "{" || s[i] == "[") {
+        stck.push(s[i])
+        // Pop the opening bracket off the stack, 
+        // if there is a corresponding closing bracket in the string
+      } else if ((char == "(" && s[i] == ")") || 
+        (char == "{" && s[i] == "}") || 
+        (char == "[" && s[i] == "]")) {
+        stck.pop()
+      } else return false
+    }
+    // Check empty stack
+      return stck.length ? false : true
+}
