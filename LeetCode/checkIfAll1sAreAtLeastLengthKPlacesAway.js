eb2361c18b1ab38a4b42d49ffc1980cc1df0f4a1
bahayg <bahayg@gmail.com>
2020-06-13 14:16:35 -0700

    Check If All 1's Are at Least Length K Places Away

---
+++ LeetCode/checkIfAll1sAreAtLeastLengthKPlacesAway.js
@@ -0,0 +1,46 @@
+// Given an array nums of 0s and 1s and an integer k, return True if all 1's are at least k places away from each other, otherwise return False.
+
+// Example 1:
+
+// Input: nums = [1,0,0,0,1,0,0,1], k = 2
+// Output: true
+// Explanation: Each of the 1s are at least 2 places away from each other.
+
+// Example 2:
+
+// Input: nums = [1,0,0,1,0,1], k = 2
+// Output: false
+// Explanation: The second 1 and third 1 are only one apart from each other.
+
+// Example 3:
+
+// Input: nums = [1,1,1,1,1], k = 0
+// Output: true
+
+// Example 4:
+
+// Input: nums = [0,1,0,1], k = 1
+// Output: true
+
+// Constraints:
+
+// 1 <= nums.length <= 10^5
+// 0 <= k <= nums.length
+// nums[i] is 0 or 1
+
+// Related Topics: Array
+
+// Hint1:
+// Each time you find a number 1, check whether or not it is K or more places away from the next one. If it's not, return false.
+
+// Runtime: 128 ms / Memory: 43 MB
+
+kLengthApart = (nums, k) => {
+    let count = k
+    for (i of nums) {
+        if (i === 1 && count < k) return false
+        i === 1 ? count = 0 : count ++
+    } 
+    return true
+}
+
