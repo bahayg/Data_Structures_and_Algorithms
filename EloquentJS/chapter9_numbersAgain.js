499e6a1bbf1e09f62264661d7545dbc41a1ed852
bahayg <bahayg@gmail.com>
2020-08-15 19:41:39 -0700

    Solution of chapter9-3 exercise

--- EloquentJS/chapter9_numbersAgain.js
+++ EloquentJS/chapter9_numbersAgain.js
@@ -23,3 +23,23 @@
 // Matching the optional sign in front of the number, as well as in front of the exponent, can be done with [+\-]? or (\+|-|) (plus, minus, or nothing).
 // The more complicated part of the exercise is the problem of matching both "5." and ".5" without also matching ".". For this, a good solution is to use the | operator to separate the two cases—either one or more digits optionally followed by a dot and zero or more digits or a dot followed by one or more digits.
 // Finally, to make the e case insensitive, either add an i option to the regular expression or use [eE].
+
+
+// EJS's Solution:
+
+// Fill in this regular expression.
+let number = /^[+\-]?(\d+(\.\d*)?|\.\d+)([eE][+\-]?\d+)?$/;
+
+// Tests:
+for (let str of ["1", "-1", "+15", "1.55", ".5", "5.",
+                 "1.3e2", "1E-4", "1e+12"]) {
+  if (!number.test(str)) {
+    console.log(`Failed to match '${str}'`);
+  }
+}
+for (let str of ["1a", "+-1", "1.2.3", "1+1", "1e4.5",
+                 ".5.", "1f5", "."]) {
+  if (number.test(str)) {
+    console.log(`Incorrectly accepted '${str}'`);
+  }
+}
