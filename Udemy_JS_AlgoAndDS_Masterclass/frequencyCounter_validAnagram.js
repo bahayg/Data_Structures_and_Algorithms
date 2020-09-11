74b54940547034f2ee1b2065672aaeab4c825541
bahayg <bahayg@gmail.com>
2020-09-11 10:21:18 -0700

    Count Unique Values

--- Udemy_JS_AlgoAndDS_Masterclass/frequencyCounter_validAnagram.js
+++ Udemy_JS_AlgoAndDS_Masterclass/frequencyCounter_validAnagram.js
@@ -1,3 +1,11 @@
+// Given two strings, write a function to determine if the second string is an anagram of the first. 
+// An anagram is a word, phrase, or name formed by rearranging the letters of another, such as cinema, formed from iceman. 
+
+// Note: You may assume the string contains only lowercase alphabets. 
+
+// Time Complexity - O(n)
+
+// My solution:
 function validAnagram(s1, s2){
     // add whatever parameters you deem necessary - good luck!
     let str1 = {}
@@ -16,4 +24,34 @@ function validAnagram(s1, s2){
     }
     return true
   } 
+
+  //   Colt Steele's Solution:
+  function validAnagram(first, second) {
+    if (first.length !== second.length) {
+      return false;
+    }
   
+    const lookup = {};
+  
+    for (let i = 0; i < first.length; i++) {
+      let letter = first[i];
+      // if letter exists, increment, otherwise set to 1
+      lookup[letter] ? lookup[letter] += 1 : lookup[letter] = 1;
+    }
+    console.log(lookup)
+  
+    for (let i = 0; i < second.length; i++) {
+      let letter = second[i];
+      // can't find letter or letter is zero then it's not an anagram
+      if (!lookup[letter]) {
+        return false;
+      } else {
+        lookup[letter] -= 1;
+      }
+    }
+  
+    return true;
+  }
+  
+  // {a: 0, n: 0, g: 0, r: 0, m: 0,s:1}
+  validAnagram('anagrams', 'nagaramm')
---
+++ Udemy_JS_AlgoAndDS_Masterclass/multiplePointers_countUniqueValues.js
@@ -0,0 +1,37 @@
+// Implement a function called countUniqueValues, which accepts a sorted array, and counts the unique values in the array. 
+// There can be negative numbers in the array, but it will always be sorted. 
+
+// Time Complexity - O(n)
+// Space Complexity - O(1)
+
+// My solution:
+function countUniqueValues(arr){
+    // add whatever parameters you deem necessary - good luck!
+    if(arr.length === 0) return 0
+    let i = 0
+    let j = 1
+    while (j<arr.length) {
+        if(arr[i] !== arr[j]) {
+            i ++
+            arr[i] = arr[j]
+            j ++
+        } else {
+            j ++
+        }
+    }
+    return i + 1
+  }
+
+//   Colt Steele's Solution:
+function countUniqueValues(arr){
+    if(arr.length === 0) return 0;
+    var i = 0;
+    for(var j = 1; j < arr.length; j++){
+        if(arr[i] !== arr[j]){
+            i++;
+            arr[i] = arr[j]
+        }
+    }
+    return i + 1;
+}
+countUniqueValues([1,2,2,5,7,7,99])
