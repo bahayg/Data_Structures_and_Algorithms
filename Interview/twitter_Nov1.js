4aa95b2e77a8fca97c3ec69811abc85e3ad3c670
bahayg <bahayg@gmail.com>
2020-10-31 12:41:38 -0700

    Add an interview question

---
+++ Interview/twitter_Nov1.js
@@ -0,0 +1,13 @@
+1. Your Life in a Tweet
+Tell us the story of your life in Tweet(s). A Tweet is an update you post on Twitter and can be up to 280 characters. 
+If needed, you may create a Tweetstorm (collection of Tweets).
+
+2. You Favorite Programming Project
+Tell us about your favorite programming project.
+
+3. A Challenge You Conquered
+Tell us about a problem that challenged you while you were learning, but you ultimately conquered?
+
+4. Autoscale Policy
+
+5. Slot Machine 2.0
