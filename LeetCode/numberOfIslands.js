b9b0f6149a09ae0ac9ded4bce5f00b04cfa9393b
bahayg <bahayg@gmail.com>
2021-05-08 18:48:24 -0700

    Number of Islands

---
+++ LeetCode/numberOfIslands.js
@@ -0,0 +1,60 @@
+// Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+// An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+// Example 1:
+// Input: grid = [
+//   ["1","1","1","1","0"],
+//   ["1","1","0","1","0"],
+//   ["1","1","0","0","0"],
+//   ["0","0","0","0","0"]
+// ]
+// Output: 1
+
+// Example 2:
+// Input: grid = [
+//   ["1","1","0","0","0"],
+//   ["1","1","0","0","0"],
+//   ["0","0","1","0","0"],
+//   ["0","0","0","1","1"]
+// ]
+// Output: 3
+
+// Constraints:
+// m == grid.length
+// n == grid[i].length
+// 1 <= m, n <= 300
+// grid[i][j] is '0' or '1'.
+
+// Related Topics: Depth-first Search / Breadth-first Search / Union Find
+
+// Runtime: 80 ms / Memory: 39.3 MB
+
+var numIslands = (grid) => {
+  let count = 0;
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      if (grid[i][j] === "1") {
+        dfs(i, j);
+        count++;
+      }
+    }
+  }
+
+  function dfs(i, j) {
+    if (
+      i < 0 ||
+      i >= grid.length ||
+      j < 0 ||
+      j >= grid[i].length ||
+      grid[i][j] === "0"
+    ) {
+      return;
+    }
+    grid[i][j] = "0";
+    dfs(i + 1, j);
+    dfs(i - 1, j);
+    dfs(i, j + 1);
+    dfs(i, j - 1);
+  }
+  return count;
+};
